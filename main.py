7841c99cc5415a29136adf57f0a0a9090ef468aa
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-27 12:49:40 -0300

    Classes for training

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ from PIL import Image
 torch.manual_seed(0)
 
 print('Using PyTorch v', torch.__version__)
+
+class_name = ['normal', 'sick', 'covid']
+root_dir = 'Covid Database'
+source_dirs = ['NORMAL', 'Viral Pneumonia', 'COVID-19']
