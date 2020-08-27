bd6a5bcbd7b3a460dede46936dc3172340129d87
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-27 13:43:20 -0300

    capituring image path

--- main.py
+++ main.py
@@ -24,3 +24,11 @@ if os.path.isdir(os.path.join((root_dir, source_dirs[1]))):
 
     for c in class_names:
         os.mkdir(os.path.join(root_dir, 'test', c))
+
+    for c in class_names:
+        images = [x for x in os.listdir(os.path.join(root_dir, c)) if x.lower().endswith('png')]
+        selected_images = random.sample(images, 30)
+        for image in selected_images:
+            source_path = os.path.join(root_dir, c, image)
+            target_path = os.path.join(root_dir, 'test', c, image)
+            shutil.move(source_path, target_path)
