fa2ee59516fe66e857efb5323d3858e6576e3d00
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-28 05:23:20 -0300

    refac

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ torch.manual_seed(0)
 
 print('Using PyTorch v', torch.__version__)
 
-class_names = ['normal', 'sick', 'covid']
+class_names = ['normal', 'viral', 'covid']
 root_dir = 'Covid Database'
 source_dirs = ['NORMAL', 'Viral Pneumonia', 'COVID-19']
 
-if os.path.isdir(os.path.join((root_dir, source_dirs[1]))):
+if os.path.isdir(os.path.join(root_dir, source_dirs[1])):
     os.mkdir(os.path.join(root_dir, 'test'))
 
     for i, d in enumerate(source_dirs):
@@ -33,28 +33,27 @@ if os.path.isdir(os.path.join((root_dir, source_dirs[1]))):
             source_path = os.path.join(root_dir, c, image)
             target_path = os.path.join(root_dir, 'test', c, image)
             shutil.move(source_path, target_path)
-            
+
+
 class ChestXRayDataset(torch.utils.data.Dataset):
     def __init__(self, image_dirs, transform):
         def get_images(class_name):
             images = [x for x in os.listdir(image_dirs[class_name]) if x[-3:].lower().endswith('png')]
             print(f'Found {len(images)} {class_name} examples')
             return images
-        
+
         self.images = {}
         self.class_names = ['normal', 'viral', 'covid']
-        
+
         for class_name in self.class_names:
             self.images[class_name] = get_images(class_name)
-            
+
         self.image_dirs = image_dirs
         self.transform = transform
-        
-    
+
     def __len__(self):
         return sum([len(self.images[class_name]) for class_name in self.class_names])
-    
-    
+
     def __getitem__(self, index):
         class_name = random.choice(self.class_names)
         index = index % len(self.images[class_name])
@@ -63,7 +62,7 @@ class ChestXRayDataset(torch.utils.data.Dataset):
         image = Image.open(image_path).convert('RGB')
         return self.transform(image), self.class_names.index(class_name)
 
-    
+
 train_transform = torchvision.transforms.Compose([
     torchvision.transforms.Resize(size=(224, 224)),
     torchvision.transforms.RandomHorizontalFlip(),
@@ -76,7 +75,7 @@ test_transform = torchvision.transforms.Compose([
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
-    
+
 train_dirs = {
     'normal': 'Covid Database/normal',
     'viral': 'Covid Database/viral',
@@ -88,7 +87,7 @@ train_dataset = ChestXRayDataset(train_dirs, train_transform)
 test_dirs = {
     'normal': 'Covid Database/test/normal',
     'viral': 'Covid Database/test/viral',
-    'covid': 'COVID-19 Radiography Database/test/covid'
+    'covid': 'Covid Database/test/covid'
 }
 
 test_dataset = ChestXRayDataset(test_dirs, test_transform)
@@ -122,3 +121,85 @@ def show_images(images, labels, preds):
         plt.ylabel(f'{class_names[int(preds[i].numpy())]}', color=col)
     plt.tight_layout()
     plt.show()
+
+
+images, labels = next(iter(dl_train))
+show_images(images, labels, labels)
+
+images, labels = next(iter(dl_test))
+show_images(images, labels, labels)
+
+resnet18 = torchvision.models.resnet18(pretrained=True)
+print(resnet18)
+
+resnet18.fc = torch.nn.Linear(in_features=512, out_features=3)
+loss_fn = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(resnet18.parameters(), lr=3e-5)
+
+
+def show_preds():
+    resnet18.eval()
+    images, labels = next(iter(dl_test))
+    outputs = resnet18(images)
+    _, preds = torch.max(outputs, 1)
+    show_images(images, labels, preds)
+
+
+show_preds()
+
+
+def train(epochs):
+    print('Starting training..')
+    for e in range(0, epochs):
+        print('=' * 20)
+        print(f'Starting epoch {e + 1}/{epochs}')
+        print('=' * 20)
+
+        train_loss = 0.
+        val_loss = 0.
+
+        resnet18.train()  # set model to training phase
+
+        for train_step, (images, labels) in enumerate(dl_train):
+            optimizer.zero_grad()
+            outputs = resnet18(images)
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+            if train_step % 20 == 0:
+                print('Evaluating at step', train_step)
+
+                accuracy = 0
+
+                resnet18.eval()  # set model to eval phase
+
+                for val_step, (images, labels) in enumerate(dl_test):
+                    outputs = resnet18(images)
+                    loss = loss_fn(outputs, labels)
+                    val_loss += loss.item()
+
+                    _, preds = torch.max(outputs, 1)
+                    accuracy += sum((preds == labels).numpy())
+
+                val_loss /= (val_step + 1)
+                accuracy = accuracy / len(test_dataset)
+                print(f'Validation Loss: {val_loss:.4f}, Accuracy: {accuracy:.4f}')
+
+                show_preds()
+
+                resnet18.train()
+
+                if accuracy >= 0.95:
+                    print('Performance condition satisfied, stopping..')
+                    return
+
+        train_loss /= (train_step + 1)
+
+        print(f'Training Loss: {train_loss:.4f}')
+    print('Training complete..')
+
+
+train(epochs=1)
+
+show_preds()
