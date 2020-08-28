fd4c837a8585a3b13060d842536bbbde9c45b79c
Leonardo Almeida de Araújo <leonardo.a@acad.ifma.edu.br>
2020-08-28 12:48:14 +0000

    train

--- main.py
+++ main.py
@@ -63,3 +63,16 @@ class ChestXRayDataset(torch.utils.data.Dataset):
         return self.transform(image), self.class_names.index(class_name)
 
     
+train_transform = torchvision.transforms.Compose([
+    torchvision.transforms.Resize(size=(224, 224)),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+test_transform = torchvision.transforms.Compose([
+    torchvision.transforms.Resize(size=(224, 224)),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+    
